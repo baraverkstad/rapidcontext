439a24530dce27425c7be28dca88877c655ca71f
Per Cederberg <per@percederberg.net>
2010-09-30 11:59:52 +0200

    Fixed file header comment.

--- src/java/org/rapidcontext/core/data/Path.java
+++ src/java/org/rapidcontext/core/data/Path.java
@@ -1,7 +1,6 @@
 /*
  * RapidContext <http://www.rapidcontext.com/>
- * Copyright (c) 2007-2010 Per Cederberg.
- * All rights reserved.
+ * Copyright (c) 2007-2010 Per Cederberg. All rights reserved.
  *
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the BSD license.
