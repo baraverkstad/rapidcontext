5e37f69e9436a7a774354375150631e226124c8c
Per Cederberg <per@percederberg.net>
2010-10-19 18:53:08 +0200

    Added new NavigationBar widget.

--- src/plugin/default/files/js/RapidContext.Widget.js
+++ src/plugin/default/files/js/RapidContext.Widget.js
@@ -1626,6 +1626,126 @@ MochiKit.Base.update(RapidContext.Widget.Icon, {
 });
 
 /**
+ * Creates a new navigation bar widget.
+ *
+ * @constructor
+ * @param {Object} attrs the widget and node attributes
+ *
+ * @return {Widget} the widget DOM node
+ *
+ * @class The navigation bar widget class. Used to show step-based
+ *     progress or location in a tree structure. In both cases, it
+ *     allows the user to go back to a previous step. The widget
+ *     uses a &lt;table&gt; HTML element.
+ * @extends RapidContext.Widget
+ * @property {Number} position The current position in the path (from 0)
+ * @property {Number} maxPosition The maximum position reached when
+ *     navigating.
+ * @property {Array} path The path being navigated.
+ *
+ * @example
+ * var nav = RapidContext.Widget.NavigationBar();
+ * nav.moveTo(0, ["Step 1", "Step 2", "Step 3"]);
+ */
+RapidContext.Widget.NavigationBar = function (attrs/*, ...*/) {
+    var tr = MochiKit.DOM.TR();
+    var tbody = MochiKit.DOM.TBODY({}, tr);
+    var o = MochiKit.DOM.TABLE({}, tbody);
+    RapidContext.Widget._widgetMixin(o, arguments.callee);
+    o.addClass("widgetNavigationBar");
+    o.setAttrs(attrs);
+    o.moveTo(0, ["Start"]);
+    o.onclick = RapidContext.Widget._eventHandler(null, "_handleClick");
+    return o;
+};
+
+/**
+ * Emitted when the position is changed, either by moving either
+ * forward or backward.
+ *
+ * @name RapidContext.Widget.NavigationBar#onchange
+ * @event
+ */
+
+/**
+ * Updates the widget or HTML DOM node attributes.
+ *
+ * @param {Object} attrs the widget and node attributes to set
+ */
+RapidContext.Widget.NavigationBar.prototype.setAttrs = function (attrs) {
+    MochiKit.DOM.updateNodeAttributes(this, attrs);
+};
+
+/**
+ * Resets the position and maximum position counters and moves to
+ * the first step in the path. 
+ */
+RapidContext.Widget.NavigationBar.prototype.reset = function () {
+    this.position = 0;
+    this.maxPosition = 0;
+    this.moveTo(0);
+};
+
+/**
+ * Moves the navigation bar to the specified position in the path.
+ * Optionally a new path may also be provided.
+ *
+ * @param {Number} pos the new position to move to (from 0)
+ * @param {Array} [path] the new path to navigate
+ */
+RapidContext.Widget.NavigationBar.prototype.moveTo = function (pos, path) {
+    this.position = pos;
+    this.maxPosition = Math.max(this.maxPosition, pos);
+    if (path != null) {
+        this.path = path.slice(0);
+    }
+    var tr = this.firstChild.firstChild;
+    MochiKit.DOM.replaceChildNodes(tr);
+    for (var i = 0; i < this.path.length; i++) {
+        var className = this._posClass(i);
+        tr.appendChild(MochiKit.DOM.TD({ "class": className }, this.path[i]));
+        className = className + "-" + this._posClass(i + 1);
+        tr.appendChild(MochiKit.DOM.TD({ "class": className }, "\xa0"));
+    }
+    RapidContext.Widget.emitSignal(this, "onchange", this);
+};
+
+/**
+ * Returns the class to use for a specified path position.
+ *
+ * @param {Number} pos the position to render
+ *
+ * @return {String} the CSS class corresponding to the position 
+ */
+RapidContext.Widget.NavigationBar.prototype._posClass = function (pos) {
+    if (pos >= this.path.length) {
+        return "end";
+    } else if (pos == this.position) {
+        return "active";
+    } else if (pos < this.maxPosition) {
+        return "prev";
+    } else {
+        return "prev";
+    }
+};
+
+/**
+ * Handles click events on the navigation bar.
+ *
+ * @param {Event} evt the MochiKit.Signal.Event object
+ */
+RapidContext.Widget.NavigationBar.prototype._handleClick = function (evt) {
+    var node = evt.target();
+    if (node.className === "prev") {
+        var step = 0;
+        while (node = node.previousSibling) {
+            step++;
+        }
+        this.moveTo(step / 2);
+    }
+};
+
+/**
  * Creates a new overlay widget.
  *
  * @constructor
@@ -4456,6 +4576,7 @@ RapidContext.Widget.Classes = {
     Form: RapidContext.Widget.Form,
     FormValidator: RapidContext.Widget.FormValidator,
     Icon: RapidContext.Widget.Icon,
+    NavigationBar: RapidContext.Widget.NavigationBar,
     Overlay: RapidContext.Widget.Overlay,
     Popup: RapidContext.Widget.Popup,
     Pane: RapidContext.Widget.Pane,
