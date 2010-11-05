b18981c28fcd1fafd2d28233adf5cc0b57470cac
Per Cederberg <per@percederberg.net>
2010-11-05 16:23:20 +0100

    Added heuristic detection of the application directory based on class path.

--- src/java/org/rapidcontext/app/ServerApplication.java
+++ src/java/org/rapidcontext/app/ServerApplication.java
@@ -1,6 +1,6 @@
 /*
  * RapidContext <http://www.rapidcontext.com/>
- * Copyright (c) 2007-2009 Per Cederberg & Dynabyte AB.
+ * Copyright (c) 2007-2010 Per Cederberg & Dynabyte AB.
  * All rights reserved.
  *
  * This program is free software: you can redistribute it and/or
@@ -22,14 +22,16 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang.SystemUtils;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.servlet.Context;
 import org.mortbay.jetty.servlet.ServletHolder;
+import org.rapidcontext.util.ClassLoaderUtil;
 
 /**
  * The stand-alone server application.
  *
- * @author   Per Cederberg, Dynabyte AB
+ * @author   Per Cederberg
  * @version  1.0
  */
 public class ServerApplication {
@@ -51,19 +53,25 @@ public class ServerApplication {
      * @param args           the command-line parameters
      */
     public static void main(String[] args) {
+        File     dir = findAppDir();
         Server   server;
         Context  root;
         int      port;
 
-        ApplicationContext.init(new File("."));
+        if (dir == null) {
+            LOG.severe("Failed to locate application directory.");
+            System.exit(1);
+        }
+        dir = dir.getAbsoluteFile();
+        ApplicationContext.init(dir);
         server = new Server(findAvailablePort());
         root = new Context(server, "/", Context.SESSIONS);
-        root.setResourceBase(".");
+        root.setResourceBase(dir.toString());
         root.getSessionHandler().getSessionManager().setMaxInactiveInterval(240 * 60);
         root.addServlet(new ServletHolder(new ServletApplication()), "/*");
         server.setStopAtShutdown(true);
         port = server.getConnectors()[0].getPort();
-        writePortFile(port);
+        writePortFile(dir, port);
         LOG.info("Starting server on localhost:" + port);
         try {
             server.start();
@@ -72,6 +80,61 @@ public class ServerApplication {
             LOG.severe("Forced shutdown.");
             System.exit(1);
         }
+    }
+
+    /**
+     * Attempts to locate the application directory based on the
+     * current working directory and the class path.
+     *
+     * @return the application directory found, or
+     *         null otherwise
+     */
+    private static File findAppDir() {
+        File[] dirs = { new File("."),
+                        SystemUtils.getUserDir(),
+                        ClassLoaderUtil.getLocation(ServerApplication.class) };
+
+        for (int i = 0; i < dirs.length; i++) {
+            File file = dirs[i];
+            for (int j = 0; file != null && j < 4; j++) {
+                if (isAppDir(file)) {
+                    return file;
+                }
+                file = file.getParentFile();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Checks if the specified file is the application directory.
+     *
+     * @param file           the file to check
+     *
+     * @return true if the file matches, or
+     *         false otherwise
+     */
+    private static boolean isAppDir(File file) {
+        return file != null &&
+               isDir(file, true) &&
+               isDir(new File(file, "plugins"), true) &&
+               isDir(new File(file, "doc"), false);
+    }
+
+    /**
+     * Checks if the specified file is a readable directory.
+     *
+     * @param file           the file to check
+     * @param write          the write check flag
+     *
+     * @return true if the file is a directory, or
+     *         false otherwise
+     */
+    private static boolean isDir(File file, boolean write) {
+        return file != null &&
+               file.isDirectory() &&
+               file.canRead() &&
+               (!write || file.canWrite());
     }
 
     /**
@@ -123,10 +186,11 @@ public class ServerApplication {
     /**
      * Creates a file containing the current server port number.
      *
+     * @param baseDir        the base directory
      * @param port           the port number
      */
-    private static void writePortFile(int port) {
-        File         dir = new File("var");
+    private static void writePortFile(File baseDir, int port) {
+        File         dir = new File(baseDir, "var");
         File         file;
         PrintWriter  os;
 
---
+++ src/java/org/rapidcontext/util/ClassLoaderUtil.java
@@ -0,0 +1,104 @@
+/*
+ * RapidContext <http://www.rapidcontext.com/>
+ * Copyright (c) 2007-2010 Per Cederberg. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the BSD license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the RapidContext LICENSE.txt file for more details.
+ */
+
+package org.rapidcontext.util;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
+
+/**
+ * A set of utility methods for working with class loaders.
+ *
+ * @author   Per Cederberg
+ * @version  1.0
+ */
+public class ClassLoaderUtil {
+
+    /**
+     * Returns the class loader for the specified class.
+     *
+     * @param cls            the class to check
+     *
+     * @return the class loader for the class, or
+     *         the system class loader if null
+     */
+    public static ClassLoader getClassLoader(Class cls) {
+        if (cls == null || cls.getClassLoader() == null) {
+            return ClassLoader.getSystemClassLoader();
+        } else {
+            return cls.getClassLoader();
+        }
+    }
+
+    /**
+     * Returns the resource URL corresponding the the specified class.
+     *
+     * @param cls            the class to check
+     *
+     * @return the resource URL for the class, or
+     *         null if not found
+     */
+    public static URL getResource(Class cls) {
+        String  name = cls.getName().replace('.', '/') + ".class";
+        return getClassLoader(cls).getResource(name);
+    }
+
+    /**
+     * Returns the file containing the specified class. This method
+     * uses the class resource URL to attempt to deduce the file
+     * system location of the class.
+     *
+     * @param cls            the class to check
+     *
+     * @return the absolute file system location for the class, or
+     *         null if not found
+     */
+    public static File getLocation(Class cls) {
+        URL     url = getResource(cls);
+        String  path;
+        String  str;
+        File    file;
+
+        if (url == null) {
+            return null;
+        }
+        path = url.toExternalForm();
+        do {
+            str = path.toLowerCase();
+            if (str.startsWith("jar:")) {
+                path = path.substring(4);
+                if (path.indexOf("!/") >= 0) {
+                    path = path.substring(0, path.indexOf("!/"));
+                }
+            } else if (str.startsWith("file:/")) {
+                path = path.substring(6);
+                if (!path.startsWith("/")) {
+                    path = "/" + path;
+                }
+                while (path.length() > 1 && path.charAt(1) == '/') {
+                    path = path.substring(1);
+                }
+            }
+        } while (path.length() < str.length());
+        try {
+            path = URLDecoder.decode(path, "UTF-8");
+        } catch (UnsupportedEncodingException ignore) {
+            // Yeah, right... That just HAD to be a checked exception.
+        }
+        file = new File(path);
+        return file.exists() ? file.getAbsoluteFile() : file;
+    }
+}
