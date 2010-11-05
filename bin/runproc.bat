b2451f145e2cf7d7e7874a8c7f82d8109b8291e7
Per Cederberg <per@percederberg.net>
2010-11-05 14:08:41 +0100

    Removed old Win9x detection, since it is no longer relevant.

--- bin/runproc.bat
+++ bin/runproc.bat
@@ -1,17 +1,11 @@
 @echo off
 
-::Check for Windows NT compliance
-> reg1.txt echo 1234&rem
-type reg1.txt | find "rem"
-if not errorlevel 1 goto ERRWIN9X
-del reg1.txt 2> nul
-
 ::Locate the application directory
 for %%x in (%0) do set BATCHPATH=%%~dpsx
 SET RAPIDCONTEXT_HOME=%BATCHPATH:~0,-5%
 cd %RAPIDCONTEXT_HOME%
 
-::Find Java and setup classpath
+::Find Java and JAR file
 CALL bin\common.bat
 if errorlevel 1 goto DONE
 
@@ -22,9 +16,5 @@ if errorlevel 1 goto DONE
 set JAVA_HOME=%_JAVA_HOME%
 set JAVA_OPTS=%_JAVA_OPTS%
 goto DONE
-
-:ERRWIN9X
-del reg1.txt 2> nul
-echo ERROR: Windows 98 or previous is not supported.
 
 :DONE
--- bin/startserver.bat
+++ bin/startserver.bat
@@ -1,17 +1,11 @@
 @echo off
 
-::Check for Windows NT compliance
-> reg1.txt echo 1234&rem
-type reg1.txt | find "rem"
-if not errorlevel 1 goto ERRWIN9X
-del reg1.txt 2> nul
-
 ::Locate the application directory
 for %%x in (%0) do set BATCHPATH=%%~dpsx
 SET RAPIDCONTEXT_HOME=%BATCHPATH:~0,-5%
 cd %RAPIDCONTEXT_HOME%
 
-::Find Java and setup classpath
+::Find Java and JAR file
 CALL bin\common.bat
 if errorlevel 1 goto DONE
 
@@ -39,10 +33,6 @@ goto DONE
 :NOPORT
 start http://localhost/
 goto DONE
-
-:ERRWIN9X
-del reg1.txt 2> nul
-echo ERROR: Windows 98 or previous is not supported.
 
 :DONE
 echo Done
