128081be6a28fdc0c5f9d6741c84474d61784960
Per Cederberg <per@percederberg.net>
2010-11-08 08:50:40 +0100

    Simplified Unix startup scripts to match new single Main class.

--- bin/runproc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-. `dirname $0`/common.sh
-"$JAVA" $JAVA_OPTS -jar lib/rapidcontext-*.jar --script "$@"
--- bin/startserver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-. `dirname $0`/common.sh
-echo "Base Dir:  " `pwd`
-echo "JAVA:       $JAVA"
-echo "JAVA_OPTS:  $JAVA_OPTS"
-echo
-mkdir -p var
-echo $$ > var/server.pid
-exec "$JAVA" $JAVA_OPTS -jar lib/rapidcontext-*.jar --server "$@"
