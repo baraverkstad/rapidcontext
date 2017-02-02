56eafeb0edd92b63f929e777ddbbaa4d201b0465
Per Cederberg <per@percederberg.net>
2017-02-02 10:09:14 +0100

    Updated copyright year to 2017.
    Also added 'ant util-copyright' target helper.

--- src/plugin/cmdline/src/org/rapidcontext/app/plugin/cmdline/CmdLineExecProcedure.java
+++ src/plugin/cmdline/src/org/rapidcontext/app/plugin/cmdline/CmdLineExecProcedure.java
@@ -1,6 +1,6 @@
 /**
  * RapidContext command-line plug-in <http://www.rapidcontext.com/>
- * Copyright (c) 2008-2013 Per Cederberg. All rights reserved.
+ * Copyright (c) 2008-2017 Per Cederberg. All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the BSD license.
--- src/plugin/jdbc/src/org/rapidcontext/app/plugin/jdbc/JdbcBuiltInStatementProcedure.java
+++ src/plugin/jdbc/src/org/rapidcontext/app/plugin/jdbc/JdbcBuiltInStatementProcedure.java
@@ -1,6 +1,6 @@
 /*
  * RapidContext JDBC plug-in <http://www.rapidcontext.com/>
- * Copyright (c) 2007-2013 Per Cederberg. All rights reserved.
+ * Copyright (c) 2007-2017 Per Cederberg. All rights reserved.
  *
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the BSD license.
