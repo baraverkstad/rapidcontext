0681e941c1f99da2cddf659aa809324681fcee03
Per Cederberg <cederberg@gmail.com>
2023-05-24 19:56:47 +0200

    js: Refactored RapidContext.Procedure module to IIFE initialization

--- src/plugin/system/files/js/RapidContext_Procedure.js
+++ src/plugin/system/files/js/RapidContext_Procedure.js
@@ -12,251 +12,254 @@
  * See the RapidContext LICENSE for more details.
  */
 
-// Create default RapidContext object
-if (typeof(RapidContext) == "undefined") {
-    RapidContext = {};
-}
+(function (window) {
 
-/**
- * Creates a new procedure caller function. This function can be called either
- * as a constructor or as a plain function. In both cases it returns a new
- * JavaScript function with additional methods.
- *
- * @constructor
- * @param {String} procedure the procedure name
- * @property {String} procedure The procedure name.
- * @property {Array} args The arguments used in the last call.
- *
- * @class The procedure wrapper function. Used to provide a simplified way of
- *   calling a procedure and connecting results through signals (instead of
- *   using promise callbacks).
- *
- *   The actual calls are performed with normal function calls, but the results
- *   are asynchronous. When called, the procedure function returns a
- *   `RapidContext.Async` promise (as the normal API call), but the results
- *   will also be signalled through the `onsuccess` signal.
- *
- *   Differing from normal functions, a procedure function will also ensure
- *   that only a single call is in progress at any time, automatically
- *   cancelling any previous call if needed.
- */
-RapidContext.Procedure = function (procedure) {
-    function self() {
-        self.args = Array.from(arguments);
-        return self.recall();
-    }
-    self.procedure = procedure;
-    self.args = null;
-    self._promise = null;
-    for (var k in RapidContext.Procedure.prototype) {
-        if (!self[k]) {
-            self[k] = RapidContext.Procedure.prototype[k];
+    /**
+     * Creates a new procedure caller function. This function can be called either
+     * as a constructor or as a plain function. In both cases it returns a new
+     * JavaScript function with additional methods.
+     *
+     * @constructor
+     * @param {String} procedure the procedure name
+     * @property {String} procedure The procedure name.
+     * @property {Array} args The arguments used in the last call.
+     *
+     * @name RapidContext.Procedure
+     * @class The procedure wrapper function. Used to provide a simplified way of
+     *   calling a procedure and connecting results through signals (instead of
+     *   using promise callbacks).
+     *
+     *   The actual calls are performed with normal function calls, but the results
+     *   are asynchronous. When called, the procedure function returns a
+     *   `RapidContext.Async` promise (as the normal API call), but the results
+     *   will also be signalled through the `onsuccess` signal.
+     *
+     *   Differing from normal functions, a procedure function will also ensure
+     *   that only a single call is in progress at any time, automatically
+     *   cancelling any previous call if needed.
+     */
+    function Procedure(procedure) {
+        function self() {
+            self.args = Array.from(arguments);
+            return self.recall();
         }
+        self.procedure = procedure;
+        self.args = null;
+        self._promise = null;
+        for (var k in Procedure.prototype) {
+            if (!self[k]) {
+                self[k] = Procedure.prototype[k];
+            }
+        }
+        return self;
     }
-    return self;
-};
 
-/**
- * Creates a new procedure caller for each key-value-pair in the specified
- * object.
- *
- * @param {Object} obj an object mapping keys to procedure names
- *
- * @return {Object} an object mapping keys to procedure instances
- */
-RapidContext.Procedure.mapAll = function (obj) {
-    var res = {};
-    for (var k in obj) {
-        res[k] = RapidContext.Procedure(obj[k]);
+    /**
+     * Emitted when the procedure is called. Each call corresponds to exactly one
+     * `oncall` and one `onresponse` event (even if the call was cancelled). No
+     * event data will be sent.
+     *
+     * @name RapidContext.Procedure#oncall
+     * @event
+     */
+
+    /**
+     * Emitted if a partial procedure result is available. This event will only be
+     * emitted when performing a multi-call, along with the `oncall` and
+     * `onresponse` events (for each call). The partial procedure result will be
+     * sent as the event data.
+     *
+     * @name RapidContext.Procedure#onupdate
+     * @event
+     */
+
+    /**
+     * Emitted when the procedure response has been received. Each call corresponds
+     * to exactly one `oncall` and one `onresponse` event (even if the call was
+     * cancelled). The call response or error object will be sent as the event
+     * data.
+     *
+     * @name RapidContext.Procedure#onresponse
+     * @event
+     */
+
+    /**
+     * Emitted when a procedure call returned a result. This event is emitted after
+     * the `onresponse` event, but only if the procedure call actually succeeded.
+     * Use the `onerror` or `oncancel` signals for other result statuses. The call
+     * response object will be sent as the event data.
+     *
+     * @name RapidContext.Procedure#onsuccess
+     * @event
+     */
+
+    /**
+     * Emitted when a procedure call failed. This event is emitted after the
+     * `onresponse` event, but only if the procedure call returned an error. Use
+     * the `onsuccess` or `oncancel` for other result statuses. The call error
+     * object will be sent as the event data.
+     *
+     * @name RapidContext.Procedure#onerror
+     * @event
+     */
+
+    /**
+     * Emitted when a procedure call was cancelled. This event is emitted after the
+     * `onresponse` event, but only if the procedure call was cancelled. Use the
+     * `onsuccess` or `onerror` for other result statuses. No event data will be
+     * sent.
+     *
+     * @name RapidContext.Procedure#oncancel
+     * @event
+     */
+
+    /**
+     * Calls the procedure with the same arguments as used in the last call. The
+     * call is asynchronous, so results will not be returned by this method.
+     * Instead the results will be available through the `onsuccess` signal, for
+     * example.
+     *
+     * Note that any previously running call will automatically be cancelled, since
+     * only a single call can be processed at any time.
+     *
+     * @methodOf RapidContext.Procedure.prototype
+     * @return {Promise} a `RapidContext.Async` promise that will resolve with
+     *         the response data or error
+     */
+    function recall() {
+        if (this.args === null) {
+            throw new Error("No arguments supplied for procedure call to " + this.procedure);
+        }
+        this.cancel();
+        MochiKit.Signal.signal(this, "oncall");
+        var cb = callback.bind(this);
+        this._promise = RapidContext.App.callProc(this.procedure, this.args).then(cb, cb);
+        return this._promise;
     }
-    return res;
-};
 
-/**
- * Emitted when the procedure is called. Each call corresponds to exactly one
- * `oncall` and one `onresponse` event (even if the call was cancelled). No
- * event data will be sent.
- *
- * @name RapidContext.Procedure#oncall
- * @event
- */
-
-/**
- * Emitted if a partial procedure result is available. This event will only be
- * emitted when performing a multi-call, along with the `oncall` and
- * `onresponse` events (for each call). The partial procedure result will be
- * sent as the event data.
- *
- * @name RapidContext.Procedure#onupdate
- * @event
- */
-
-/**
- * Emitted when the procedure response has been received. Each call corresponds
- * to exactly one `oncall` and one `onresponse` event (even if the call was
- * cancelled). The call response or error object will be sent as the event
- * data.
- *
- * @name RapidContext.Procedure#onresponse
- * @event
- */
-
-/**
- * Emitted when a procedure call returned a result. This event is emitted after
- * the `onresponse` event, but only if the procedure call actually succeeded.
- * Use the `onerror` or `oncancel` signals for other result statuses. The call
- * response object will be sent as the event data.
- *
- * @name RapidContext.Procedure#onsuccess
- * @event
- */
-
-/**
- * Emitted when a procedure call failed. This event is emitted after the
- * `onresponse` event, but only if the procedure call returned an error. Use
- * the `onsuccess` or `oncancel` for other result statuses. The call error
- * object will be sent as the event data.
- *
- * @name RapidContext.Procedure#onerror
- * @event
- */
-
-/**
- * Emitted when a procedure call was cancelled. This event is emitted after the
- * `onresponse` event, but only if the procedure call was cancelled. Use the
- * `onsuccess` or `onerror` for other result statuses. No event data will be
- * sent.
- *
- * @name RapidContext.Procedure#oncancel
- * @event
- */
-
-/**
- * Calls the procedure with the same arguments as used in the last call. The
- * call is asynchronous, so results will not be returned by this method.
- * Instead the results will be available through the `onsuccess` signal, for
- * example.
- *
- * Note that any previously running call will automatically be cancelled, since
- * only a single call can be processed at any time.
- *
- * @return {Promise} a `RapidContext.Async` promise that will resolve with
- *         the response data or error
- */
-RapidContext.Procedure.prototype.recall = function () {
-    if (this.args === null) {
-        throw new Error("No arguments supplied for procedure call to " + this.procedure);
-    }
-    this.cancel();
-    MochiKit.Signal.signal(this, "oncall");
-    var cb = this._callback.bind(this);
-    this._promise = RapidContext.App.callProc(this.procedure, this.args).then(cb, cb);
-    return this._promise;
-};
-
-/**
- * The procedure promise callback handler. Dispatches the appropriate signals
- * depending on the result.
- *
- * @param {Object/Error} res the procedure result object
- */
-RapidContext.Procedure.prototype._callback = function (res) {
-    this._promise = null;
-    MochiKit.Signal.signal(this, "onresponse", res);
-    if (res instanceof Error) {
-        MochiKit.Signal.signal(this, "onerror", res);
-        return Promise.reject(res);
-    } else {
-        MochiKit.Signal.signal(this, "onsuccess", res);
-        return res;
-    }
-};
-
-/**
- * Calls the procedure multiple times (in sequence) with different arguments
- * (supplied as an array of argument arrays). The calls are asynchronous, so
- * results will not be returned by this method. Instead an array with the
- * results will be available through the `onupdate` and `onsuccess` signals,
- * for example.
- *
- * Note that any previously running call will automatically be cancelled, since
- * only a single call can be processed at any time. A result `transform`
- * function can be supplied to transform each individual result. If the
- * `transform` function throws an error, that result will be omitted.
- *
- * @param {Array} args the array of argument arrays
- * @param {Function} [transform] the optional result transform function
- *
- * @return {Promise} a `RapidContext.Async` promise that will resolve with
- *         the response data or error
- */
-RapidContext.Procedure.prototype.multicall = function (args, transform) {
-    this.cancel();
-    this._mapArgs = args;
-    this._mapPos = 0;
-    this._mapRes = [];
-    this._mapTransform = transform;
-    this._nextCall();
-};
-
-/**
- * The multicall promise callback handler. Dispatches the appropriate signals
- * depending on the result.
- *
- * @param {Object/Error} res the procedure result object
- */
-RapidContext.Procedure.prototype._nextCall = function (res) {
-    this._promise = null;
-    if (typeof(res) != "undefined") {
+    // The procedure promise callback handler. Dispatches the appropriate
+    // signals depending on the result.
+    function callback(res) {
+        this._promise = null;
         MochiKit.Signal.signal(this, "onresponse", res);
         if (res instanceof Error) {
             MochiKit.Signal.signal(this, "onerror", res);
             return Promise.reject(res);
         } else {
-            if (this._mapTransform == null) {
-                this._mapRes.push(res);
-            } else {
-                try {
-                    res = this._mapTransform(res);
-                    this._mapRes.push(res);
-                } catch (ignore) {
-                    // Skip results with mapping errors
-                }
-            }
-            MochiKit.Signal.signal(this, "onupdate", this._mapRes);
+            MochiKit.Signal.signal(this, "onsuccess", res);
+            return res;
         }
     }
-    if (this._mapPos < this._mapArgs.length) {
-        this.args = this._mapArgs[this._mapPos++];
-        MochiKit.Signal.signal(this, "oncall");
-        var cb = this._nextCall.bind(this);
-        this._promise = RapidContext.App.callProc(this.procedure, this.args).then(cb, cb);
-        return this._promise;
-    } else {
-        MochiKit.Signal.signal(this, "onsuccess", this._mapRes);
-        return this._mapRes;
-    }
-};
 
-/**
- * Cancels any current execution of this procedure. This method does nothing if
- * no procedure call was currently executing.
- */
-RapidContext.Procedure.prototype.cancel = function () {
-    if (this._promise !== null) {
-        this._promise.cancel();
+    /**
+     * Calls the procedure multiple times (in sequence) with different arguments
+     * (supplied as an array of argument arrays). The calls are asynchronous, so
+     * results will not be returned by this method. Instead an array with the
+     * results will be available through the `onupdate` and `onsuccess` signals,
+     * for example.
+     *
+     * Note that any previously running call will automatically be cancelled, since
+     * only a single call can be processed at any time. A result `transform`
+     * function can be supplied to transform each individual result. If the
+     * `transform` function throws an error, that result will be omitted.
+     *
+     * @methodOf RapidContext.Procedure.prototype
+     * @param {Array} args the array of argument arrays
+     * @param {Function} [transform] the optional result transform function
+     * @return {Promise} a `RapidContext.Async` promise that will resolve with
+     *         the response data or error
+     */
+    function multicall(args, transform) {
+        this.cancel();
+        this._mapArgs = args;
+        this._mapPos = 0;
+        this._mapRes = [];
+        this._mapTransform = transform;
+        nextCall.call(this);
+    }
+
+    // The multicall promise callback handler. Dispatches the appropriate
+    // signals depending on the result.
+    function nextCall(res) {
         this._promise = null;
-        return true;
-    } else {
-        return false;
+        if (typeof res != "undefined") {
+            MochiKit.Signal.signal(this, "onresponse", res);
+            if (res instanceof Error) {
+                MochiKit.Signal.signal(this, "onerror", res);
+                return Promise.reject(res);
+            } else {
+                if (this._mapTransform == null) {
+                    this._mapRes.push(res);
+                } else {
+                    try {
+                        res = this._mapTransform(res);
+                        this._mapRes.push(res);
+                    } catch (ignore) {
+                        // Skip results with mapping errors
+                    }
+                }
+                MochiKit.Signal.signal(this, "onupdate", this._mapRes);
+            }
+        }
+        if (this._mapPos < this._mapArgs.length) {
+            this.args = this._mapArgs[this._mapPos++];
+            MochiKit.Signal.signal(this, "oncall");
+            var cb = nextCall.bind(this);
+            this._promise = RapidContext.App.callProc(this.procedure, this.args).then(cb, cb);
+            return this._promise;
+        } else {
+            MochiKit.Signal.signal(this, "onsuccess", this._mapRes);
+            return this._mapRes;
+        }
     }
-};
 
-/**
- * Cancels any current execution and removes the reference to the arguments of
- * this procedure.
- */
-RapidContext.Procedure.prototype.reset = function () {
-    this.cancel();
-    this.args = null;
-};
+    /**
+     * Cancels any current execution of this procedure. This method does nothing if
+     * no procedure call was currently executing.
+     *
+     * @methodOf RapidContext.Procedure.prototype
+     */
+    function cancel() {
+        if (this._promise !== null) {
+            this._promise.cancel();
+            this._promise = null;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Cancels any current execution and removes the reference to the arguments of
+     * this procedure.
+     *
+     * @methodOf RapidContext.Procedure.prototype
+     */
+    function reset() {
+        this.cancel();
+        this.args = null;
+    }
+
+    /**
+     * Creates a new procedure caller for each key-value-pair in the specified
+     * object.
+     *
+     * @memberOf RapidContext.Procedure
+     * @param {Object} obj an object mapping keys to procedure names
+     * @return {Object} an object mapping keys to procedure instances
+     */
+    function mapAll(obj) {
+        var res = {};
+        for (var k in obj) {
+            res[k] = Procedure(obj[k]);
+        }
+        return res;
+    }
+
+    // Create namespace and export API
+    var RapidContext = window.RapidContext || (window.RapidContext = {});
+    RapidContext.Procedure = Procedure;
+    Object.assign(Procedure.prototype, { recall, multicall, cancel, reset });
+    Object.assign(Procedure, { mapAll });
+
+})(this);
