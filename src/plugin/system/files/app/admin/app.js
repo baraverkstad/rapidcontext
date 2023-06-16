baraverkstad/rapidcontext

commit 2cf45d5b95fa3c8999fb16bfd322b1afb074d4c7

js: Replaced usage of bind(), partial() and similar with arrow functions

src/plugin/system/files/app/admin/app.js
@@ -12,7 +12,7 @@ function AdminApp() {
         delay: 5,
         queue: [],
         stat: { success: 0, failed: 0 },
-        func: MochiKit.Base.bind("_processBatch", this)
+        func: () => this._processBatch()
     };
 }
 
@@ -35,7 +35,7 @@ AdminApp.prototype.start = function () {
     });
 
     // All views
-    MochiKit.Signal.connect(this.ui.root, "onenter", MochiKit.Base.bind("selectChild", this.ui.tabContainer, null));
+    MochiKit.Signal.connect(this.ui.root, "onenter", () => this.ui.tabContainer.selectChild(null));
     MochiKit.Signal.connect(this.proc.typeList, "onsuccess", this, "_updateTypeCache");
 
     // Connection view
@@ -227,8 +227,8 @@ AdminApp.prototype._validateConnections = function () {
             return Promise.allSettled(data.map(validate));
         })
         .catch(function () {})
-        .then(this.proc.cxnList.recall.bind(this.proc.cxnList))
-        .finally(this.ui.overlay.hide.bind(this.ui.overlay));
+        .then(() => this.proc.cxnList.recall())
+        .finally(() => this.ui.overlay.hide());
 };
 
 /**
@@ -243,8 +243,8 @@ AdminApp.prototype.showConnection = function (id) {
     this.ui.tabContainer.selectChild(this.ui.cxnTab);
     RapidContext.App.callProc("system/connection/validate", [id])
         .catch(function () {})
-        .then(this.proc.cxnList.recall.bind(this.proc.cxnList))
-        .then(this.ui.cxnTable.setSelectedIds.bind(this.ui.cxnTable, id));
+        .then(() => this.proc.cxnList.recall())
+        .then(() => this.ui.cxnTable.setSelectedIds(id));
 };
 
 /**
@@ -314,7 +314,7 @@ AdminApp.prototype._removeConnection = function () {
     if (confirm(msg)) {
         var path = RapidContext.Storage.path(data);
         RapidContext.App.callProc("system/storage/write", [path, null])
-            .then(this.proc.cxnList.recall.bind(this.proc.cxnList))
+            .then(() => this.proc.cxnList.recall())
             .catch(RapidContext.UI.showError);
     }
 };
@@ -351,8 +351,8 @@ AdminApp.prototype._initConnectionEdit = function (data) {
                 }
             }
         })
-        .then(this._updateConnectionEdit.bind(this))
-        .then(this.ui.cxnEditDialog.show.bind(this.ui.cxnEditDialog))
+        .then(() => this._updateConnectionEdit())
+        .then(() => this.ui.cxnEditDialog.show())
         .catch(RapidContext.UI.showError);
 };
 
@@ -485,8 +485,8 @@ AdminApp.prototype._storeConnection = function () {
             path = { path: oldPath, updateTo: path };
         }
         RapidContext.App.callProc("system/storage/write", [path, data])
-            .then(this.ui.cxnEditDialog.hide.bind(this.ui.cxnEditDialog))
-            .then(this.showConnection.bind(this, data.id))
+            .then(() => this.ui.cxnEditDialog.hide())
+            .then(() => this.showConnection(data.id))
             .catch(RapidContext.UI.showError);
     }
 };
@@ -598,8 +598,8 @@ AdminApp.prototype._togglePlugin = function () {
     RapidContext.App.callProc(proc, [data.id])
         .catch(RapidContext.UI.showError)
         .then((data.loaded && data.className) ? showRestartMesssage : function () {})
-        .then(this.resetServer.bind(this))
-        .then(this.loadPlugins.bind(this));
+        .then(() => this.resetServer())
+        .then(() => this.loadPlugins());
 };
 
 /**
@@ -643,8 +643,8 @@ AdminApp.prototype._pluginUpload = function () {
         .then(function (res) {
             pluginId = res;
         })
-        .then(this.resetServer.bind(this))
-        .then(this.loadPlugins.bind(this))
+        .then(() => this.resetServer())
+        .then(() => this.loadPlugins())
         .then(select.bind(this))
         .catch(RapidContext.UI.showError)
         .finally(done.bind(this));
@@ -722,7 +722,7 @@ AdminApp.prototype._showProcedure = function () {
     this.ui.procExecResult.removeAll();
     RapidContext.Util.resizeElements(this.ui.procExecResult);
     if (node != null && node.data != null) {
-        var cb = this._callbackShowProcedure.bind(this, node.data);
+        var cb = (res) => this._callbackShowProcedure(node.data, res);
         this.proc.procRead(node.data).then(cb, cb);
         this.ui.procLoading.show();
     }
@@ -766,7 +766,7 @@ AdminApp.prototype._callbackShowProcedure = function (procName, res) {
                     "class": "font-smaller ml-1",
                     "icon": "fa fa-lg fa-pencil"
                 });
-                btn.onclick = MochiKit.Base.bind("_editProcArg", this, count);
+                btn.onclick = () => this._editProcArg(count);
                 var col2 = MochiKit.DOM.TD({ "class": "text-nowrap pr-2" }, field, btn);
                 var col3 = MochiKit.DOM.TD({ "class": "text-pre-wrap w-100 pt-1" }, b.description);
                 var tr = MochiKit.DOM.TR({}, col1, col2, col3);
@@ -862,7 +862,7 @@ AdminApp.prototype._addProcedure = function () {
         bindings: {},
         defaults: {}
     };
-    this._initProcEdit(data).then(this._updateProcEdit.bind(this));
+    this._initProcEdit(data).then(() => this._updateProcEdit());
 };
 
 /**
@@ -919,8 +919,8 @@ AdminApp.prototype._initProcEdit = function (data) {
                 data.defaults[k] = RapidContext.Util.dict(keys, values);
             });
         })
-        .then(this._renderProcEdit.bind(this))
-        .then(this.ui.procEditDialog.show.bind(this.ui.procEditDialog))
+        .then(() => this._renderProcEdit())
+        .then(() => this.ui.procEditDialog.show())
         .catch(RapidContext.UI.showError);
 };
 
@@ -1087,9 +1087,9 @@ AdminApp.prototype._saveProcedure = function () {
         path = { path: oldPath, updateTo: path };
     }
     RapidContext.App.callProc("system/storage/write", [path, data])
-        .then(this.ui.procEditDialog.hide.bind(this.ui.procEditDialog))
-        .then(this.proc.procList.recall.bind(this.proc.procList))
-        .then(this.showProcedure.bind(this, data.id))
+        .then(() => this.ui.procEditDialog.hide())
+        .then(() => this.proc.procList.recall())
+        .then(() => this.showProcedure(data.id))
         .catch(RapidContext.UI.showError);
 };
 
@@ -1121,7 +1121,7 @@ AdminApp.prototype._executeProcedure = function () {
     this.ui.procExec.disable();
     this.ui.procBatch.disable();
     this.ui.procExecResult.removeAll();
-    var cb = this._callbackExecute.bind(this);
+    var cb = (res) => this._callbackExecute(res);
     RapidContext.App.callProc(proc.name, args).then(cb, cb);
 };
 
@@ -1378,7 +1378,7 @@ AdminApp.prototype._processBatch = function () {
         if (item == null) {
             this._stopBatch();
         } else {
-            var cb = this._callbackBatch.bind(this);
+            var cb = (res) => this._callbackBatch(res);
             RapidContext.App.callProc(item.proc, item.args).then(cb, cb);
         }
     }

src/plugin/system/files/app/help/app.js
@@ -217,9 +217,9 @@ HelpApp.prototype.loadContent = function (url) {
         var source = (node && node.data) ? node.data.source || "" : "";
         MochiKit.DOM.replaceChildNodes(this.ui.contentInfo, source);
         RapidContext.App.loadText(fileUrl)
-            .then(this._callbackContent.bind(this))
+            .then((data) => this._callbackContent(data))
             .catch(RapidContext.UI.showError)
-            .finally(this.ui.contentLoading.hide.bind(this.ui.contentLoading));
+            .finally(() => this.ui.contentLoading.hide());
     }
 };
 

src/plugin/system/files/app/login/app.js
@@ -32,8 +32,8 @@ LoginApp.prototype._loginAuth = function () {
     this.ui.loginAuth.setAttrs({ disabled: true, icon: "LOADING" });
     var data = this.ui.loginForm.valueMap();
     RapidContext.App.login($.trim(data.user), data.password)
-        .then(window.location.reload.bind(window.location))
-        .catch(this._loginError.bind(this));
+        .then(() => window.location.reload())
+        .catch((err) => this._loginError(err));
 };
 
 /**

src/plugin/system/files/app/start/app.js
@@ -367,7 +367,7 @@ StartApp.prototype._loginOut = function () {
 StartApp.prototype._loginAuth = function () {
     this.ui.loginAuth.setAttrs({ disabled: true, icon: "fa fa-spin fa-refresh" });
     var data = this.ui.loginForm.valueMap();
-    var cb = this._loginAuthCallback.bind(this);
+    var cb = (res) => this._loginAuthCallback(res);
     RapidContext.App.login($.trim(data.user), data.password).then(cb, cb);
 };
 
@@ -424,18 +424,18 @@ StartApp.prototype._tourChange = function () {
     var promise = RapidContext.Async.wait(0);
     switch (this.ui.tourWizard.activePageIndex()) {
     case 1:
-        promise = promise.then(this._tourLocateStart.bind(this));
+        promise = promise.then(() => this._tourLocateStart());
         break;
     case 2:
         promise = RapidContext.App.callApp("help", "loadTopics")
-            .then(RapidContext.Async.wait.bind(null, 1))
-            .then(this._tourLocateHelp.bind(this));
+            .then(() => RapidContext.Async.wait(1))
+            .then(() => this._tourLocateHelp());
         break;
     case 3:
-        promise = promise.then(this._tourLocateTabs.bind(this));
+        promise = promise.then(() => this._tourLocateTabs());
         break;
     case 4:
-        promise = promise.then(this._tourLocateUser.bind(this));
+        promise = promise.then(() => this._tourLocateUser());
         break;
     }
     return promise.catch(RapidContext.UI.showError);
@@ -474,8 +474,7 @@ StartApp.prototype._tourLocateTabs = function () {
 StartApp.prototype._tourLocateUser = function () {
     if (this.ui.menu.isHidden()) {
         this.ui.menu.show();
-        var func = MochiKit.Base.bind("_tourLocateUser", this);
-        setTimeout(func, 500);
+        setTimeout(() => this._tourLocateUser(), 500);
     } else {
         this.ui.menu.show();
         this._tourLocate(this.ui.menu);

src/plugin/test/files/app/example/app.js
@@ -29,7 +29,7 @@ ExampleApp.prototype.start = function () {
     // Initialize data
     this.proc.appList();
     this.initIcons();
-    this.interval = setInterval(MochiKit.Base.method(this, "progressUpdate"), 500);
+    this.interval = setInterval(() => this.progressUpdate(), 500);
 };
 
 /**
@@ -102,7 +102,7 @@ ExampleApp.prototype.initIcons = function () {
         MochiKit.DOM.appendChildNodes(tr, TD({ "class": "end" }));
         this.ui.iconTable.appendChild(tr);
     }
-    this.ui.iconTable.resizeContent = MochiKit.Base.noop;
+    this.ui.iconTable.resizeContent = () => {};
 };
 
 /**

src/plugin/system/files/js/RapidContext.Util.js
@@ -465,7 +465,7 @@ RapidContext.Util.registerSizeConstraints = function (node, width, height) {
  * ==> Evaluates the size constraints for a node and all child nodes
  *
  * @example
- * elem.resizeContent = MochiKit.Base.noop;
+ * elem.resizeContent = () => {};
  * ==> Assigns a no-op child resize handler to elem
  */
 RapidContext.Util.resizeElements = function (/* ... */) {

src/plugin/system/files/js/RapidContext_App.js
@@ -52,7 +52,7 @@ RapidContext.App.init = function (app) {
 
     // Setup UI
     RapidContext.Util.registerSizeConstraints(document.body, "100%-20", "100%-20");
-    var resizer = MochiKit.Base.partial(RapidContext.Util.resizeElements, document.body);
+    var resizer = () => RapidContext.Util.resizeElements(document.body);
     MochiKit.Signal.connect(window, "onresize", resizer);
     RapidContext.Util.resizeElements(document.body);
     var overlay = new RapidContext.Widget.Overlay({ message: "Loading..." });
@@ -211,7 +211,7 @@ RapidContext.App.startApp = function (app, container) {
     function load(launcher) {
         RapidContext.Log.info("Loading app/" + launcher.id + " resources", launcher);
         launcher.resource = {};
-        var promises = launcher.resources.map(loadResource.bind(null, launcher));
+        var promises = launcher.resources.map((res) => loadResource(launcher, res));
         return Promise.all(promises).then(function () {
             launcher.creator = launcher.creator || window[launcher.className];
             if (launcher.creator == null) {
@@ -240,7 +240,7 @@ RapidContext.App.startApp = function (app, container) {
     function launch(launcher, ui) {
         RapidContext.Log.context("RapidContext.App.startApp(" + launcher.id + ")");
         return RapidContext.Async.wait(0)
-            .then((launcher.creator == null) ? load.bind(null, launcher) : function () {})
+            .then(() => launcher.creator ? true : load(launcher))
             .then(function () {
                 RapidContext.Log.info("Starting app/" + launcher.id, launcher);
                 /* eslint new-cap: "off" */
@@ -251,7 +251,7 @@ RapidContext.App.startApp = function (app, container) {
                 delete props.instances;
                 MochiKit.Base.setdefault(instance, props);
                 MochiKit.Signal.disconnectAll(ui.root, "onclose");
-                var halt = MochiKit.Base.partial(RapidContext.App.stopApp, instance);
+                var halt = () => RapidContext.App.stopApp(instance);
                 MochiKit.Signal.connect(ui.root, "onclose", halt);
                 if (launcher.ui != null) {
                     buildUI(ui.root, ui, launcher.ui);
@@ -302,13 +302,12 @@ RapidContext.App.startApp = function (app, container) {
         } else if (instances.length > 0) {
             // Switch from single-app to multi-app mode
             var elems = Array.from(document.body.childNodes);
-            var moveApp = moveAppToStart.bind(null, instances[0], elems);
             var overlay = new RapidContext.Widget.Overlay({ message: "Loading..." });
             document.body.insertBefore(overlay, document.body.childNodes[0]);
-            var promise = launch(start, { root: document.body, overlay: overlay }).then(moveApp);
-            // FIXME: double calls to this ?
-            var recall = RapidContext.App.startApp.bind(null, launcher.id, container);
-            resolve((launcher.id === "start") ? promise : promise.then(recall));
+            let ui = { root: document.body, overlay: overlay };
+            var move = () => moveAppToStart(instances[0], elems);
+            var recall = () => (launcher.id === "start") ? true : RapidContext.App.startApp(launcher.id);
+            resolve(launch(start, ui).then(move).then(recall));
         } else {
             // Launch single-app mode
             var ui = { root: document.body, overlay: document.body.childNodes[0] };

src/plugin/system/files/js/RapidContext_Async.js
@@ -94,8 +94,7 @@
          */
         then: function (onFulfilled, onRejected) {
             var promise = wrapPromise(this, onFulfilled, onRejected);
-            var onCancel = this.cancel.bind(this);
-            return new Async(promise, onCancel);
+            return new Async(promise, () => this.cancel());
         },
 
         /**
@@ -120,8 +119,7 @@
          */
         finally: function (onFinally) {
             var promise = this._promise.finally(onFinally);
-            var onCancel = this.cancel.bind(this);
-            return new Async(promise, onCancel);
+            return new Async(promise, () => this.cancel());
         },
 
         /**
@@ -232,9 +230,9 @@
     function wait(millis, value) {
         var timer = null;
         function callLater(resolve) {
-            timer = setTimeout(resolve.bind(null, value), millis);
+            timer = setTimeout(() => resolve(value), millis);
         }
-        return new Async(callLater, clearTimeout.bind(null, timer));
+        return new Async(callLater, () => clearTimeout(timer));
     }
 
     /**
@@ -339,7 +337,7 @@
             xhr.send(opts.body);
         });
         var cancel = function () {
-            xhr && setTimeout(xhr.abort.bind(xhr));
+            xhr && setTimeout(() => xhr.abort());
             xhr = null;
         };
         return new Async(promise, cancel);

commit bbb8ccfb45c8f59bd8b02a3cb695e7e914f8a6dd

js: Fixed admin app argument editing (broken since 2cf45d5b95)

src/plugin/system/files/app/admin/app.js
@@ -768,7 +768,7 @@ AdminApp.prototype._callbackShowProcedure = function (procName, res) {
                     "class": "font-smaller ml-1",
                     "icon": "fa fa-lg fa-pencil"
                 });
-                btn.onclick = () => this._editProcArg(count);
+                btn.onclick = this._editProcArg.bind(this, count);
                 var col2 = MochiKit.DOM.TD({ "class": "text-nowrap pr-2" }, field, btn);
                 var col3 = MochiKit.DOM.TD({ "class": "text-pre-wrap w-100 pt-1" }, b.description);
                 var tr = MochiKit.DOM.TR({}, col1, col2, col3);