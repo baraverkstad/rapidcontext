d7539ec216732b9722a7a306d989420b97518359
Per Cederberg <cederberg@gmail.com>
2023-07-06 06:26:42 +0200

    legacy: Updated source code copyright year

--- src/plugin/legacy/files/js/zzz-legacy/RapidContext.Util.Old.js
+++ src/plugin/legacy/files/js/zzz-legacy/RapidContext.Util.Old.js
@@ -1,6 +1,6 @@
 /*
  * RapidContext <https://www.rapidcontext.com/>
- * Copyright (c) 2007-2012 Per Cederberg. All rights reserved.
+ * Copyright (c) 2007-2023 Per Cederberg. All rights reserved.
  *
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the BSD license.
