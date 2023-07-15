e3e92af30c816571dd0f34c94b06ce401faf43ec
Per Cederberg <cederberg@gmail.com>
2023-07-15 08:39:51 +0200

    http: Fixed incorrect source code file comments

--- src/plugin/http/src/org/rapidcontext/app/plugin/http/HttpRequestProcedure.java
+++ src/plugin/http/src/org/rapidcontext/app/plugin/http/HttpRequestProcedure.java
@@ -1,9 +1,9 @@
-/**
+/*
  * RapidContext HTTP plug-in <https://www.rapidcontext.com/>
  * Copyright (c) 2007-2023 Per Cederberg. All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the BSD license.
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the BSD license.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
