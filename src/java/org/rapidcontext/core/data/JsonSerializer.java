0c3116003661f214ef62e4bf968ef154e3377b80
Per Cederberg <cederberg@gmail.com>
2023-11-01 07:34:08 +0100

    core: Changed JSON serialization to use compact format if indented
    This partially reverts d72dc4c9 and 18542d6f, as the JSON object will
    no longer be used for serialization. Instead, the older code has been
    brought back and modified to detect possible one-line array and dict
    structures.

--- src/java/org/rapidcontext/core/data/JsonSerializer.java
+++ src/java/org/rapidcontext/core/data/JsonSerializer.java
@@ -18,11 +18,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.Date;
 
-import org.rapidcontext.core.js.JsRuntime;
-import org.rapidcontext.util.FileUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.mozilla.javascript.Function;
 import org.rapidcontext.core.js.JsException;
+import org.rapidcontext.core.js.JsRuntime;
+import org.rapidcontext.util.DateUtil;
+import org.rapidcontext.util.FileUtil;
 
 /**
  * A data serializer and unserializer for the JSON format. The object
@@ -32,17 +35,14 @@ import org.rapidcontext.core.js.JsException;
  *
  * <ul>
  *   <li>No circular references are permitted.
- *   <li>String, Integer, Boolean, Array, Dict and StorableObject
- *       values are supported.
+ *   <li>String, Number, Boolean, Date, Array and Dict values are
+ *       supported.
  * </ul>
  *
  * @author   Per Cederberg
  * @version  1.0
  */
 public final class JsonSerializer {
-
-    // The compiled serialize function
-    private static Function serializeFunction = null;
 
     // The compiled unserialize function
     private static Function unserializeFunction = null;
@@ -74,35 +74,174 @@ public final class JsonSerializer {
      * @return a JSON data representation
      */
     public static String serialize(Object obj, boolean indent) {
-        try {
-            if (serializeFunction == null) {
-                String[] args = new String[] { "val", "indent" };
-                String body = "return JSON.stringify(val, null, indent);";
-                serializeFunction = JsRuntime.compile("serialize", args, body);
-            }
-            Object[] args = new Object[] { obj, indent ? 2 : 0 };
-            String str = JsRuntime.call(serializeFunction, args, null).toString();
-            return toPrintableAscii(str);
-        } catch (JsException ignore) {
-            return "null";
+        StringBuilder buffer = new StringBuilder();
+        serialize(obj, indent ? 0 : -1, buffer);
+        return buffer.toString();
+    }
+
+    /**
+     * Serializes an object into a JavaScript literal (JSON). The
+     * string returned can be used as a constant inside JavaScript
+     * code or returned via a JSON API. If the indentation flag is
+     * set, the JSON data will be indented and formatted. Otherwise
+     * a minimal string will be created. The serialized result will
+     * be written into the specified string buffer.
+     *
+     * @param obj            the object to convert, or null
+     * @param indent         the indentation level, or -1 for none
+     * @param buffer         the string buffer to append into
+     */
+    private static void serialize(Object obj, int indent, StringBuilder buffer) {
+        if (obj == null) {
+            buffer.append("null");
+        } else if (obj instanceof Dict) {
+            serialize((Dict) obj, indent, buffer);
+        } else if (obj instanceof Array) {
+            serialize((Array) obj, indent, buffer);
+        } else if (obj instanceof Boolean) {
+            buffer.append(obj.toString());
+        } else if (obj instanceof Number) {
+            boolean isInt = ((Number) obj).doubleValue() % 1 == 0;
+            buffer.append(isInt ? ((Number) obj).intValue() : obj);
+        } else if (obj instanceof Date) {
+            String str = DateUtil.asEpochMillis((Date) obj);
+            buffer.append(TextEncoding.encodeJsonString(str));
+        } else if (obj instanceof Class) {
+            String str = ((Class<?>) obj).getName();
+            buffer.append(TextEncoding.encodeJsonString(str));
+        } else {
+            buffer.append(TextEncoding.encodeJsonString(obj.toString()));
         }
     }
 
+    /**
+     * Serializes a dictionary into a JavaScript literal (JSON). The
+     * string returned can be used as a constant inside JavaScript
+     * code or returned via a JSON API. If the indentation flag is
+     * set, the JSON data will be indented and formatted. Otherwise
+     * a minimal string will be created. The serialized result will
+     * be written into the specified string buffer.
+     *
+     * @param dict           the dictionary to convert
+     * @param indent         the indentation level, or -1 for none
+     * @param buffer         the string buffer to append into
+     */
+    private static void serialize(Dict dict, int indent, StringBuilder buffer) {
+        int next = indent;
+        String prefix = "";
+        String infix = ":";
+        int col = Math.max(0, buffer.length() - buffer.lastIndexOf("\n"));
+        if (indent > 0 && serializedWidth(dict, col, 0) <= 120) {
+            next = -1;
+            prefix = " ";
+            infix += " ";
+        } else if (indent >= 0) {
+            next = indent + 1;
+            prefix = "\n" + StringUtils.repeat("  ", indent + 1);
+            infix += " ";
+        }
+        String[] keys = dict.keys();
+        buffer.append("{");
+        for (int i = 0; i < keys.length; i++) {
+            if (i > 0) {
+                buffer.append(",");
+            }
+            buffer.append(prefix);
+            buffer.append(TextEncoding.encodeJsonString(keys[i]));
+            buffer.append(infix);
+            serialize(dict.get(keys[i]), next, buffer);
+        }
+        if (keys.length > 0 && next >= 0) {
+            buffer.append("\n");
+            buffer.append(StringUtils.repeat("  ", indent));
+        } else if (keys.length > 0 && indent >= 0) {
+            buffer.append(" ");
+        }
+        buffer.append("}");
+    }
 
     /**
-     * Encodes all non-printable ASCII characters in a JSON string.
+     * Serializes an array into a JavaScript literal (JSON). The
+     * string returned can be used as a constant inside JavaScript
+     * code or returned via a JSON API. If the indentation flag is
+     * set, the JSON data will be indented and formatted. Otherwise
+     * a minimal string will be created. The serialized result will
+     * be written into the specified string buffer.
      *
-     * @param str            the text to encode
-     *
-     * @return the encoded JSON string
+     * @param arr            the array to convert
+     * @param indent         the indentation level, or -1 for none
+     * @param buffer         the string buffer to append into
      */
-    private static String toPrintableAscii(String str) {
-        StringBuilder buffer = new StringBuilder();
-        for (int i = 0; str != null && i < str.length(); i++) {
-            char c = str.charAt(i);
-            buffer.append((c < 127) ? c : String.format("\\u%04x", Integer.valueOf(c)));
+    private static void serialize(Array arr, int indent, StringBuilder buffer) {
+        int next = indent;
+        String prefix = "", suffix = "", infix = "";
+        int col = Math.max(0, buffer.length() - buffer.lastIndexOf("\n"));
+        if (indent > 0 && serializedWidth(arr, col, 0) <= 120) {
+            next = -1;
+            prefix = suffix = "";
+            infix = " ";
+        } else if (indent >= 0) {
+            next = indent + 1;
+            prefix = infix = "\n" + StringUtils.repeat("  ", indent + 1);
+            suffix = "\n" + StringUtils.repeat("  ", indent);
         }
-        return buffer.toString();
+        buffer.append("[");
+        buffer.append(prefix);
+        for (int i = 0; i < arr.size(); i++) {
+            if (i > 0) {
+                buffer.append(",");
+                buffer.append(infix);
+            }
+            serialize(arr.get(i), next, buffer);
+        }
+        buffer.append(suffix);
+        buffer.append("]");
+    }
+
+    /**
+     * Returns the approximate column after serializing an object.
+     * If the nesting is too deep or the column size becomes larger
+     * than 120, the estimation is aborted.
+     *
+     * @param obj            the object to serialize, or null
+     * @param pos            the current line position (column)
+     * @param depth          the current item depth
+     *
+     * @return the approximate maximum line position (column) reached
+     */
+    private static int serializedWidth(Object obj, int pos, int depth) {
+        if (obj == null || obj instanceof Boolean || obj instanceof Number) {
+            return pos + 4;
+        } else if (obj instanceof Date) {
+            return pos + 16;
+        } else if (obj instanceof Dict && depth > 1) {
+            return Integer.MAX_VALUE;
+        } else if (obj instanceof Dict) {
+            Dict d = (Dict) obj;
+            pos += 4;
+            for (String key : d.keys()) {
+                pos += 5 + key.length();
+                pos = serializedWidth(d.get(key), pos, depth + 1);
+                if (pos > 120) {
+                    break;
+                }
+            }
+            return pos;
+        } else if (obj instanceof Array && depth > 1) {
+            return Integer.MAX_VALUE;
+        } else if (obj instanceof Array) {
+            pos += 2;
+            for (Object item : (Array) obj) {
+                pos += 2;
+                pos = serializedWidth(item, pos, depth + 1);
+                if (pos > 120) {
+                    break;
+                }
+            }
+            return pos;
+        } else {
+            return pos + 2 + obj.toString().length();
+        }
     }
 
     /**
