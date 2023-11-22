e0d387860fde8747067d3960eb212fcbe1ccae87
Per Cederberg <cederberg@gmail.com>
2023-11-22 19:27:53 +0100

    core: Added FIXME comment for HttpOnly cookies

--- src/java/org/rapidcontext/core/web/Request.java
+++ src/java/org/rapidcontext/core/web/Request.java
@@ -835,6 +835,7 @@ public class Request implements HttpUtil {
         Cookie cookie = new Cookie(SESSION_COOKIE, Objects.toString(sessionId, "deleted"));
         cookie.setPath(StringUtils.prependIfMissing(path, "/"));
         cookie.setSecure(request.isSecure());
+        // FIXME: Add when Jetty is updated: cookie.setHttpOnly(true);
         cookie.setMaxAge((sessionId == null) ? 0 : (int) Session.MAX_AGE_MILLIS / 1000);
         response.addCookie(cookie);
     }
