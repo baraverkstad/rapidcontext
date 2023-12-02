25070500a0b3aa800ed8f6dfc9fbe8c1cd630045
Per Cederberg <cederberg@gmail.com>
2023-12-02 19:26:57 +0100

    test: Updated ExampleApp to newer components and styles

--- src/plugin/test/files/app/example/app.js
+++ src/plugin/test/files/app/example/app.js
@@ -13,12 +13,11 @@ window.ExampleApp = class {
         });
 
         // Attach signal handlers
+        this.ui.zooPane.on("click", "[data-action]", (evt) => this.action(evt));
         MochiKit.Signal.connect(this.ui.progressForm, "onclick", this, "progressConfig");
         RapidContext.UI.connectProc(this.proc.appList, this.ui.appLoading, this.ui.appReload);
         MochiKit.Signal.connect(this.proc.appList, "oncall", this.ui.appTable, "clear");
         MochiKit.Signal.connect(this.proc.appList, "onsuccess", this.ui.appTable, "setData");
-        MochiKit.Signal.connect(this.ui.dialogButton, "onclick", this.ui.dialog, "show");
-        MochiKit.Signal.connect(this.ui.dialogClose, "onclick", this.ui.dialog, "hide");
         MochiKit.Signal.connect(this.ui.iconShowAll, "onchange", this, "toggleIcons");
 
         // Initialize data
@@ -31,6 +30,34 @@ window.ExampleApp = class {
     stop() {
         // Cleanup any resources or handlers
         clearInterval(this.interval);
+    }
+
+    action(evt) {
+        switch (evt.delegateTarget.dataset.action) {
+        case "dlg-show":
+            this.ui.dialog.show();
+            break;
+        case "msg-error":
+            RapidContext.UI.Msg.error("Test error");
+            break;
+        case "msg-warning":
+            RapidContext.UI.Msg.warning("Test warning");
+            break;
+        case "msg-success":
+            RapidContext.UI.Msg.success("Test success");
+            break;
+        case "msg-info":
+            RapidContext.UI.Msg.info("Test info");
+            break;
+        case "show-overlay":
+            this.ui.overlay.setAttrs({ message: "Test overlay..." });
+            this.ui.overlay.show();
+            setTimeout(() => this.ui.overlay.hide(), 3000);
+            break;
+        case "hide-overlay":
+            this.ui.overlay.hide();
+            break;
+        }
     }
 
     // Modify the progress bar style
