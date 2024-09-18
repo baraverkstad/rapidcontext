baraverkstad/rapidcontext

commit d43e73c7ead9d7d23be4fd4ba077e885a19468ec

core: Use Java pattern matching with instanceof operator

src/java/org/rapidcontext/core/data/JsonSerializer.java
@@ -96,24 +96,24 @@ public final class JsonSerializer {
     private static void serialize(Object obj, int indent, StringBuilder buffer) {
         if (obj == null) {
             buffer.append("null");
-        } else if (obj instanceof Dict) {
-            serialize((Dict) obj, indent, buffer);
-        } else if (obj instanceof Array) {
-            serialize((Array) obj, indent, buffer);
-        } else if (obj instanceof Map) {
-            serialize(Dict.from((Map<?, ?>) obj), indent, buffer);
-        } else if (obj instanceof Iterable) {
-            serialize(Array.from((Iterable<?>) obj), indent, buffer);
+        } else if (obj instanceof Dict d) {
+            serialize(d, indent, buffer);
+        } else if (obj instanceof Array a) {
+            serialize(a, indent, buffer);
+        } else if (obj instanceof Map<?,?> m) {
+            serialize(Dict.from(m), indent, buffer);
+        } else if (obj instanceof Iterable<?> i) {
+            serialize(Array.from(i), indent, buffer);
         } else if (obj instanceof Boolean) {
             buffer.append(obj.toString());
-        } else if (obj instanceof Number) {
-            buffer.append(StringUtils.removeEnd(obj.toString(), ".0"));
-        } else if (obj instanceof Date) {
-            String str = DateUtil.asEpochMillis((Date) obj);
+        } else if (obj instanceof Number n) {
+            boolean isInt = n.doubleValue() % 1 == 0;
+            buffer.append(isInt ? n.intValue() : obj);
+        } else if (obj instanceof Date dt) {
+            String str = DateUtil.asEpochMillis(dt);
             buffer.append(TextEncoding.encodeJsonString(str));
-        } else if (obj instanceof Class) {
-            String str = ((Class<?>) obj).getName();
-            buffer.append(TextEncoding.encodeJsonString(str));
+        } else if (obj instanceof Class<?> c) {
+            buffer.append(TextEncoding.encodeJsonString(c.getName()));
         } else {
             buffer.append(TextEncoding.encodeJsonString(obj.toString()));
         }
@@ -221,8 +221,7 @@ public final class JsonSerializer {
             return pos + 16;
         } else if (obj instanceof Dict && depth > 1) {
             return Integer.MAX_VALUE;
-        } else if (obj instanceof Dict) {
-            Dict d = (Dict) obj;
+        } else if (obj instanceof Dict d) {
             pos += 4;
             for (String key : d.keys()) {
                 pos += 5 + key.length();
@@ -234,9 +233,9 @@ public final class JsonSerializer {
             return pos;
         } else if (obj instanceof Array && depth > 1) {
             return Integer.MAX_VALUE;
-        } else if (obj instanceof Array) {
+        } else if (obj instanceof Array a) {
             pos += 2;
-            for (Object item : (Array) obj) {
+            for (Object item : a) {
                 pos += 2;
                 pos = serializedWidth(item, pos, depth + 1);
                 if (pos > 120) {

src/java/org/rapidcontext/app/model/ApiUtil.java
@@ -59,11 +59,7 @@ public class ApiUtil {
      * @return an options dictionary
      */
     public static Dict options(String defaultKey, Object value) {
-        if (value instanceof Dict) {
-            return (Dict) value;
-        } else {
-            return new Dict().set(defaultKey, value);
-        }
+        return (value instanceof Dict d) ? d : new Dict().set(defaultKey, value);
     }
 
     /**
@@ -168,8 +164,8 @@ public class ApiUtil {
             LOG.fine("updating storage path " + src + (rename ? " -> " + dst : ""));
             Object prev = StorableObject.sterilize(storage.load(src), false, true, false);
             patch = (Dict) StorableObject.sterilize(patch, false, true, true);
-            if (prev instanceof Dict) {
-                storage.store(dst, ((Dict) prev).merge(patch));
+            if (prev instanceof Dict d) {
+                storage.store(dst, d.merge(patch));
                 if (rename) {
                     storage.remove(src);
                 }
@@ -243,25 +239,23 @@ public class ApiUtil {
                                    Object obj,
                                    Dict opts,
                                    boolean limitedTypes) {
-        if (obj instanceof Index) {
-            Index idx = (Index) obj;
+        if (obj instanceof Index i) {
             boolean hidden = opts.get("hidden", Boolean.class, false);
             return new Dict()
                 .set("type", "index")
                 .set("modified", Objects.requireNonNullElse(idx.modified(), new Date()))
                 .set("paths", Array.from(idx.paths(path, hidden)));
-        } else if (obj instanceof Binary) {
-            Binary data = (Binary) obj;
+        } else if (obj instanceof Binary b) {
             Dict dict = new Dict()
                 .set("type", "file")
                 .set("path", path)
                 .set("name", path.name())
-                .set("mimeType", data.mimeType())
-                .set("modified", new Date(data.lastModified()))
-                .set("size", data.size());
+                .set("mimeType", b.mimeType())
+                .set("modified", new Date(b.lastModified()))
+                .set("size", b.size());
             if (opts.get("computed", Boolean.class, false)) {
-                try (InputStream is = data.openStream()) {
-                    if (Mime.isText(data.mimeType())) {
+                try (InputStream is = b.openStream()) {
+                    if (Mime.isText(b.mimeType())) {
                         dict.set("_text", FileUtil.readText(is));
                     } else {
                         dict.set("_data", Base64.getEncoder().encodeToString(is.readAllBytes()));

src/java/org/rapidcontext/app/proc/AppListProcedure.java
@@ -102,7 +102,7 @@ public class AppListProcedure extends Procedure {
         Array arr = dict.getArray("resources");
         for (int i = 0; i < arr.size(); i++) {
             Object obj = arr.get(i);
-            Dict resource = (obj instanceof Dict) ? (Dict) obj : new Dict();
+            Dict resource = (obj instanceof Dict d) ? d : new Dict();
             String url = resource.get("url", String.class, obj.toString());
             if (!url.contains("//:") && StringUtils.containsAny(url, "*?")) {
                 arr.remove(i--);

src/java/org/rapidcontext/app/proc/ConnectionListProcedure.java
@@ -77,8 +77,8 @@ public class ConnectionListProcedure extends Procedure {
                 Dict dict = null;
                 if (o instanceof Connection) {
                     dict = (Dict) StorableObject.sterilize(o, true, false, true);
-                } else if (o instanceof Dict) {
-                    dict = (Dict) o;
+                } else if (o instanceof Dict d) {
+                    dict = d;
                     if (!dict.containsKey("_error")) {
                         String msg = "failed to initialize: plug-in for " +
                                      "connnection type probably not loaded";

src/java/org/rapidcontext/app/proc/ProcedureCallProcedure.java
@@ -64,8 +64,8 @@ public class ProcedureCallProcedure extends Procedure {
         CallContext.checkAccess("procedure/" + name, cx.readPermission(1));
         Object[] args = null;
         Object obj = bindings.getValue("arguments");
-        if (obj instanceof Array) {
-            args = ((Array) obj).values();
+        if (obj instanceof Array a) {
+            args = a.values();
         } else {
             args = new Object[1];
             args[0] = obj;

src/java/org/rapidcontext/app/proc/ProcedureReadProcedure.java
@@ -93,12 +93,11 @@ public class ProcedureReadProcedure extends Procedure {
         Path path = Path.resolve(Library.PATH_PROC, proc.getName());
         Metadata meta = storage.lookup(Path.resolve(storagePath, path));
         Dict res = new Dict();
-        if (proc instanceof StorableObject) {
-            StorableObject storable = (StorableObject) proc;
-            res.setAll(storable.serialize());
+        if (proc instanceof StorableObject o) {
+            res.setAll(o.serialize());
             res.remove(KEY_BINDING);
-        } else if (proc instanceof AddOnProcedure) {
-            res.set("type", ((AddOnProcedure) proc).getType());
+        } else if (proc instanceof AddOnProcedure p) {
+            res.set("type", p.getType());
         } else {
             res.set("type", "built-in");
         }

src/java/org/rapidcontext/app/proc/StorageCopyProcedure.java
@@ -162,8 +162,8 @@ public class StorageCopyProcedure extends Procedure {
             }
         }
         Object data = storage.load(src);
-        if (data instanceof Binary) {
-            try (InputStream is = ((Binary) data).openStream()) {
+        if (data instanceof Binary b) {
+            try (InputStream is = b.openStream()) {
                 File file = FileUtil.tempFile(src.name());
                 FileUtil.copy(is, file);
                 storage.store(dst, file);

src/java/org/rapidcontext/app/proc/ThreadCreateProcedure.java
@@ -83,8 +83,8 @@ public class ThreadCreateProcedure extends Procedure {
         CallContext.checkAccess("procedure/" + proc, cx.readPermission(1));
         Object[] args = null;
         Object obj = bindings.getValue("arguments");
-        if (obj instanceof Array) {
-            args = ((Array) obj).values();
+        if (obj instanceof Array a) {
+            args = a.values();
         } else {
             args = new Object[1];
             args[0] = obj;

commit 5212f0c7f151a6a8f001f6208044b401f850064e

core: Fixed JSON serialization of long values (regression since d43e73c7)

src/java/org/rapidcontext/core/data/JsonSerializer.java
@@ -107,8 +107,7 @@ public final class JsonSerializer {
         } else if (obj instanceof Boolean) {
             buffer.append(obj.toString());
         } else if (obj instanceof Number n) {
-            boolean isInt = n.doubleValue() % 1 == 0;
-            buffer.append(isInt ? n.intValue() : obj);
+            buffer.append(StringUtils.removeEnd(n.toString(), ".0"));
         } else if (obj instanceof Date dt) {
             String str = DateUtil.asEpochMillis(dt);
             buffer.append(TextEncoding.encodeJsonString(str));