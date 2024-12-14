aa580c65cf9aaddcc1093cf814198cd2f98c4a4e
Per Cederberg <cederberg@gmail.com>
2024-12-14 14:53:34 +0100

    js: Refactored login app to use JS class syntax

--- src/plugin/system/files/app/login/app.js
+++ src/plugin/system/files/app/login/app.js
@@ -1,48 +1,48 @@
-/**
- * Creates a new login app.
- */
-function LoginApp() {
+class LoginApp {
+
+    /**
+     * Starts the app and initializes the UI.
+     */
+    start() {
+        MochiKit.Signal.connect(this.ui.loginForm, "onsubmit", this, "_loginAuth");
+        let user = RapidContext.App.user();
+        if (user && user.id) {
+            $(this.ui.loginName).text(user.name || user.id);
+            $(this.ui.loginWarning).removeClass("hidden");
+        }
+        this.ui.loginDialog.show();
+        this.ui.loginUser.focus();
+    }
+
+    /**
+     * Stops the app.
+     */
+    stop() {
+        // Nothing to do here
+    }
+
+    /**
+     * Shows the login authentication dialog.
+     */
+    _loginAuth() {
+        this.ui.loginAuth.setAttrs({ disabled: true, icon: "LOADING" });
+        let data = this.ui.loginForm.valueMap();
+        RapidContext.App.login($.trim(data.user), data.password)
+            .then(() => window.location.reload())
+            .catch((err) => this._loginError(err));
+    }
+
+    /**
+     * Handles login errors.
+     */
+    _loginError(err) {
+        this.ui.loginAuth.setAttrs({ disabled: false, icon: "OK" });
+        let msg = (err && err.message) || String(err);
+        msg = msg.charAt(0).toUpperCase() + msg.substr(1);
+        $(this.ui.loginError).removeClass("hidden").text(msg);
+        $(this.ui.loginWarning).addClass("hidden");
+    }
 }
 
-/**
- * Starts the app and initializes the UI.
- */
-LoginApp.prototype.start = function () {
-    MochiKit.Signal.connect(this.ui.loginForm, "onsubmit", this, "_loginAuth");
-    var user = RapidContext.App.user();
-    if (user && user.id) {
-        $(this.ui.loginName).text(user.name || user.id);
-        $(this.ui.loginWarning).removeClass("hidden");
-    }
-    this.ui.loginDialog.show();
-    this.ui.loginUser.focus();
-};
-
-/**
- * Stops the app.
- */
-LoginApp.prototype.stop = function () {
-    // Nothing to do here
-};
-
-/**
- * Shows the login authentication dialog.
- */
-LoginApp.prototype._loginAuth = function () {
-    this.ui.loginAuth.setAttrs({ disabled: true, icon: "LOADING" });
-    var data = this.ui.loginForm.valueMap();
-    RapidContext.App.login($.trim(data.user), data.password)
-        .then(() => window.location.reload())
-        .catch((err) => this._loginError(err));
-};
-
-/**
- * Handles login errors.
- */
-LoginApp.prototype._loginError = function (err) {
-    this.ui.loginAuth.setAttrs({ disabled: false, icon: "OK" });
-    var msg = (err && err.message) || String(err);
-    msg = msg.charAt(0).toUpperCase() + msg.substr(1);
-    $(this.ui.loginError).removeClass("hidden").text(msg);
-    $(this.ui.loginWarning).addClass("hidden");
-};
+// FIXME: Switch to module and export class instead
+window.LoginApp = LoginApp;
