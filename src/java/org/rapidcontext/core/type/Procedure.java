baraverkstad/rapidcontext

commit 54b416678ece2ffc6bed3dd4eb0c8c20b3f516cc

core: Refactored procedure metrics init and reporting to avoid Library

src/java/org/rapidcontext/core/type/Procedure.java
@@ -78,6 +78,11 @@ public abstract class Procedure extends StorableObject {
     public static final long ACTIVE_MILLIS = 5L * DateUtils.MILLIS_PER_MINUTE;
 
     /**
+     * The shared metrics to use for all procedures.
+     */
+    private static Metrics metrics = null;
+
+    /**
      * Returns a stream of all procedures found in the storage.
      *
      * @param storage        the storage to search
@@ -99,6 +104,21 @@ public abstract class Procedure extends StorableObject {
      */
     public static Procedure find(Storage storage, String id) {
         return storage.load(Path.resolve(PATH, id), Procedure.class);
+    }
+
+    /**
+     * Returns the procedure usage metrics. The metrics will be loaded
+     * from storage if not already in memory.
+     *
+     * @param storage        the storage to load from
+     *
+     * @return the connection usage metrics
+     */
+    public static Metrics metrics(Storage storage) {
+        if (metrics == null) {
+            metrics = Metrics.findOrCreate(storage, "connection");
+        }
+        return metrics;
     }
 
     /**
@@ -216,4 +236,22 @@ public abstract class Procedure extends StorableObject {
      */
     public abstract Object call(CallContext cx, Bindings bindings)
         throws ProcedureException;
+
+    /**
+     * Reports connection usage metrics for a single query/request/etc.
+     * If the start time isn't positive, no actual usage and duration
+     * will be reported, only potential errors.
+     *
+     * @param start          the start time (in millis), or zero (0) for none
+     * @param success        the success flag
+     * @param error          the optional error message
+     */
+    public void report(long start, boolean success, String error) {
+        if (metrics != null) {
+            long now = System.currentTimeMillis();
+            int duration = (start <= 0) ? 0 : (int) (now - start);
+            int count = (start <= 0 && success) ? 0 : 1; // validate doesn't count
+            metrics.report(id(), now, count, duration, success, error);
+        }
+    }
 }

src/java/org/rapidcontext/app/ApplicationContext.java
@@ -44,6 +44,7 @@ import org.rapidcontext.core.storage.Storage;
 import org.rapidcontext.core.storage.StorageException;
 import org.rapidcontext.core.type.Connection;
 import org.rapidcontext.core.type.Environment;
+import org.rapidcontext.core.type.Procedure;
 import org.rapidcontext.core.type.Session;
 import org.rapidcontext.core.type.Type;
 import org.rapidcontext.core.type.WebMatcher;
@@ -328,6 +329,7 @@ public class ApplicationContext {
         // FIXME: Why is pre-loading of all types necessary?
         Type.all(storage).forEach(o -> { /* Force refresh cached types */ });
         Connection.metrics(storage); // Load or create connection metrics
+        Procedure.metrics(storage); // Load or create procedure metrics
         scheduler.submit(() -> {
             // FIXME: Move aliases into storage catalog
             library.refreshAliases();

src/java/org/rapidcontext/app/proc/ProcedureMetricsProcedure.java
@@ -62,7 +62,7 @@ public class ProcedureMetricsProcedure extends Procedure {
 
         CallContext.checkSearchAccess("procedure/");
         Dict res = new Dict();
-        cx.getLibrary().getMetrics().stream()
+        Procedure.metrics(cx.getStorage()).stream()
             .filter(e -> SecurityContext.hasReadAccess("procedure/" + e.getKey()))
             .forEach(e -> res.set(e.getKey(), e.getValue().values()));
         return res;

src/java/org/rapidcontext/core/proc/DefaultInterceptor.java
@@ -106,11 +106,11 @@ public class DefaultInterceptor extends Interceptor {
             cx.logCall(proc.id(), bindings);
             Object obj = proc.call(cx, bindings);
             cx.logResponse(obj);
-            cx.getLibrary().report(proc, start, true, null);
+            proc.report(start, true, null);
             return obj;
         } catch (ProcedureException e) {
             cx.logError(e);
-            cx.getLibrary().report(proc, start, false, e.toString());
+            proc.report(start, false, e.toString());
             throw e;
         }
     }

src/java/org/rapidcontext/core/proc/Library.java
@@ -17,7 +17,6 @@ package org.rapidcontext.core.proc;
 import java.util.HashMap;
 
 import org.rapidcontext.core.storage.Storage;
-import org.rapidcontext.core.type.Metrics;
 import org.rapidcontext.core.type.Procedure;
 
 /**
@@ -56,11 +55,6 @@ public class Library {
     private Interceptor interceptor = new DefaultInterceptor();
 
     /**
-     * The procedure call metrics.
-     */
-    private Metrics metrics = null;
-
-    /**
      * Creates a new procedure library.
      *
      * @param storage        the data storage to use
@@ -77,7 +71,7 @@ public class Library {
         // FIXME: This is very slow when a large number of procedures are available...
         aliases.clear();
         Procedure.all(storage).forEach(p -> {
-            if (p.alias() != null && !p.alias().isEmpty()) {
+            if (p.alias() != null && !p.alias().isBlank()) {
                 aliases.put(p.alias(), p.id());
             }
         });
@@ -150,37 +144,6 @@ public class Library {
             traces.put(name, Boolean.TRUE);
         } else {
             traces.remove(name);
-        }
-    }
-
-    /**
-     * Returns the procedure usage metrics.
-     *
-     * @return the procedure usage metrics
-     */
-    public Metrics getMetrics() {
-        if (metrics == null) {
-            synchronized (this) {
-                metrics = Metrics.findOrCreate(storage, "procedure");
-            }
-        }
-        return metrics;
-    }
-
-    /**
-     * Reports procedure usage metrics for a single call.
-     *
-     * @param proc           the procedure executed
-     * @param start          the start time (in millis)
-     * @param success        the success flag
-     * @param error          the optional error message
-     */
-    public void report(Procedure proc, long start, boolean success, String error) {
-        getMetrics();
-        if (metrics != null) {
-            long now = System.currentTimeMillis();
-            int duration = (int) (System.currentTimeMillis() - start);
-            metrics.report(proc.id(), now, 1, duration, success, error);
         }
     }
 }

commit 631dcaa45783cafab53825a4e957a2723050f271

core: Fixed procedure metrics retrieval (returning connection metrics)
Fixes bug introduced in 54b416678ece2ffc6bed3dd4eb0c8c20b3f516cc

src/java/org/rapidcontext/core/type/Procedure.java
@@ -145,7 +145,7 @@ public abstract class Procedure extends StorableObject {
      */
     public static Metrics metrics(Storage storage) {
         if (metrics == null) {
-            metrics = Metrics.findOrCreate(storage, "connection");
+            metrics = Metrics.findOrCreate(storage, "procedure");
         }
         return metrics;
     }