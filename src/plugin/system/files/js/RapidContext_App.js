baraverkstad/rapidcontext

commit 4061142b5e8daf264dad056d7e386b1d2e62cd99

app: Fixed race condition in call to RapidContext.App.startApp/callApp
When calling callApp() before startApp() had completely launched the
app, the instance method could be called before an async start method
had completed.

src/plugin/system/files/js/RapidContext_App.js
@@ -232,7 +232,7 @@ RapidContext.App.startApp = function (app, container) {
     }
     function launch(launcher, ui) {
         RapidContext.Log.context(`RapidContext.App.startApp(${launcher.id})`);
-        return RapidContext.Async.wait(0)
+        return launcher.starter = RapidContext.Async.wait(0)
             .then(() => launcher.creator ? true : load(launcher))
             .then(function () {
                 console.info(`Starting app/${launcher.id}`, launcher);
@@ -241,7 +241,10 @@ RapidContext.App.startApp = function (app, container) {
                 launcher.instances.push(instance);
                 const props = MochiKit.Base.setdefault({ ui: ui }, launcher);
                 delete props.creator;
+                delete props.starter;
                 delete props.instances;
+                delete props.sort;
+                delete props.launch;
                 MochiKit.Base.setdefault(instance, props);
                 MochiKit.Signal.disconnectAll(ui.root, "onclose");
                 const halt = () => RapidContext.App.stopApp(instance);
@@ -377,9 +380,8 @@ RapidContext.App.callApp = function (app, method) {
             console.error(msg, app);
             throw new Error([msg, ": ", app].join(""));
         }
-        const exists = launcher.instances.length > 0;
-        let promise = exists ? RapidContext.Async.wait(0) : RapidContext.App.startApp(app);
-        promise = promise.then(function () {
+        let starter = launcher.instances[0] || launcher.starter || RapidContext.App.startApp(app);
+        let promise = Promise.resolve(starter).then(function () {
             RapidContext.Log.context(`RapidContext.App.callApp(${launcher.id},${method})`);
             const pos = launcher.instances.indexOf(app);
             const instance = (pos >= 0) ? app : launcher.instances[launcher.instances.length - 1];

commit ddba674503510e7f1f5af5eded59eeec92e08270

js: Fixed UI timing issue caused by 4061142b
Some UI events (onchange) are apparently triggered upon creation.
Re-adding a wait(0) call between UI creation and start() call to
not have event handlers attached.

src/plugin/system/files/js/RapidContext_App.js
@@ -254,7 +254,8 @@ RapidContext.App.startApp = function (app, container) {
                 }
                 ui.overlay.hide();
                 ui.overlay.setAttrs({ message: "Working..." });
-                return RapidContext.App.callApp(instance, "start");
+                return RapidContext.Async.wait(0) // Wait for initial UI events
+                    .then(() => RapidContext.App.callApp(instance, "start"));
             })
             .catch(function (e) {
                 console.error("Failed to start app", e);
@@ -273,6 +274,7 @@ RapidContext.App.startApp = function (app, container) {
                 return Promise.reject(e);
             })
             .finally(function () {
+                delete launcher.starter;
                 RapidContext.Log.context(null);
             });
     }