53456d57d3d1ef466c12efd66caaf855f11f6b5a
Per Cederberg <cederberg@gmail.com>
2025-11-16 18:56:13 +0100

    app: Removed unused import

--- src/java/org/rapidcontext/app/ApplicationContext.java
+++ src/java/org/rapidcontext/app/ApplicationContext.java
@@ -21,7 +21,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
